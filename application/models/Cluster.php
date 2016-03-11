5984c7599f834e6d0bdd60b56d42493dcb7afd3a
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-11 12:08:56 +0800

    specified which columns can be edited/updated

--- application/models/Cluster.php
+++ application/models/Cluster.php
@@ -3,6 +3,7 @@
 class Cluster extends Eloquent {
 
     public static $table = 'graduate_cluster';
+    public static $accessible = array('cluster_name', 'email', 'max_seats', 'level', 'building', 'image_path');
 
 }
 
