129bc90bde6cbd300ea8bbf8d60092d0bd276a8b
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-11 12:08:12 +0800

    updated cluster controller so it is easier to read

--- application/controllers/cluster.php
+++ application/controllers/cluster.php
@@ -6,8 +6,9 @@ class Cluster_Controller extends Base_Controller {
 
     // renders list of all clusters
     public function get_index(){
+        $clusters = Cluster::order_by('cluster_name')->get();
         return View::make('cluster.index')
-            ->with('clusters',Cluster::order_by('cluster_name')->get());
+            ->with('clusters',$clusters);
     }
 
     // renders new page for creating cluster space  
@@ -18,12 +19,33 @@ class Cluster_Controller extends Base_Controller {
     // creates an entry in cluster table
     public function post_create(){
         insertData(Input::all());
-        return Redirect::to_route('clusters')->with('message', 'Success!');
+        $message = 'Success!';
+        return Redirect::to_route('clusters')
+            ->with('message', $message);
     }
 
+    // creates an edit page for individual author
+    public function get_edit($id){
+        $cluster = Cluster::find($id);
+        return View::make('cluster.edit')
+            ->with('cluster', $cluster);
+    }
+
+    // updates individual cluster pages
+    public function put_update(){
+        updateData(Input::all());
+        $message = "Cluster Updated!";
+        return Redirect::to_route('clusters')
+            ->with('message', $message);
+            
+    }
+
+    // deletes an entry from the cluster table
     public function delete_remove(){
-        removeCluster(Input::all());
-        return Redirect::to_route('clusters')->with('message', 'Cluster Removed!');
+        removeData(Input::all());
+        $message = 'Cluster Removed!';
+        return Redirect::to_route('clusters')
+            ->with('message', $message);
     }
 }
 
@@ -38,9 +60,19 @@ function insertData($input){
     ));        
 }
 
-function removeCluster($input){
-    Cluster::find($input['id'])->delete();
+function updateData($input){
+    $cluster_id = $input['clus'];
+    Cluster::update($cluster_id, array(
+        'cluster_name'=>$input['clusname'],
+        'email'=>$input['clusmail'],
+        'max_seats'=>$input['clusseats'],
+        'level'=>$input['cluslev'],
+        'building'=>$input['clusbuild'],
+    ));
 }
 
+function removeData($input){
+    Cluster::find($input['id'])->delete();
+}
 
 ?>
