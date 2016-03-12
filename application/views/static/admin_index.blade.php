98fc6c22dd0aaf0481b2a55df6592e8124495e4d
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-12 13:47:31 +0800

    expanded button to fill div width

--- application/views/static/admin_index.blade.php
+++ application/views/static/admin_index.blade.php
@@ -1,22 +1,24 @@
 @layout('base.default')
 
 @section('mainbody')
-  <div class="row">
-    <div class="small-7 large-centered columns">
-     <div class="row">
-       <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-       <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-     </div>
+  <div class="page-centre">
+    <div class="row">
+      <div class="small-7 large-centered columns">
+	<div class="row">
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button expand')) }}</div>
+	</div>
+      </div>
     </div>
-   </div>
 
-   <div class="row">
-     <div class="small-7 large-centered columns">
-       <div class="row">
-	 <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'REPORT PAGE', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-	 <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'SEND EMAILS', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-       </div>
-     </div>
-   </div>
+    <div class="row">
+      <div class="small-7 large-centered columns">
+	<div class="row">
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'REPORT PAGE', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'SEND EMAILS', array(), array('class'=>'button expand')) }}</div>
+	</div>
+      </div>
+    </div>
+  </div>
 
 @endsection
--- application/views/static/index.blade.php
+++ application/views/static/index.blade.php
@@ -1,22 +1,25 @@
 @layout('base.default')
 
 @section('mainbody')
-  <div class="row">
-    <div class="small-7 large-centered columns">
-     <div class="row">
-       <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'NEW BOOKING', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-       <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'CHANGE OF DESK', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-     </div>
-    </div>
-   </div>
 
-   <div class="row">
-     <div class="small-7 large-centered columns">
-       <div class="row">
-	 <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'UPDATE BOOKING', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-	 <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'ADMIN', array(), array('class'=>'button', 'style'=>'width: 100%')) }}</div>
-       </div>
-     </div>
-   </div>
+  <div class="page-centre">
+    <div class="row">
+      <div class="small-7 large-centered columns">
+	<div class="row">
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'NEW BOOKING', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'CHANGE OF DESK', array(), array('class'=>'button expand')) }}</div>
+	</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="small-7 large-centered columns">
+	<div class="row">
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'UPDATE BOOKING', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'ADMIN', array(), array('class'=>'button expand')) }}</div>
+	</div>
+      </div>
+    </div>
+  </div>
 
 @endsection
