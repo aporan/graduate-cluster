840a6a2558593b9af0f2e60c1adac9938c2d4593
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-16 02:50:24 +0800

    updated index page navigation routings

--- application/views/static/index.blade.php
+++ application/views/static/index.blade.php
@@ -6,8 +6,8 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'NEW BOOKING', array(), array('class'=>'button expand')) }}</div>
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'CHANGE OF DESK', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_booking', 'NEW BOOKING', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('bookings', 'VIEW BOOKINGS', array(), array('class'=>'button expand')) }}</div>
 	</div>
       </div>
     </div>
@@ -15,7 +15,7 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'UPDATE BOOKING', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('index', 'CHANGE OF DESK', array(), array('class'=>'button expand')) }}</div>
 	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'ADMIN', array(), array('class'=>'button expand')) }}</div>
 	</div>
       </div>
