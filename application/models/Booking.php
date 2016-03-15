032cc89eabca6db284331391c6002f63b1d30d40
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-16 02:43:01 +0800

    added student booking model

---
+++ application/models/Booking.php
@@ -0,0 +1,10 @@
+<?php
+
+class Booking extends Eloquent {
+
+    public static $table = 'student_booking';
+    public static $accessbile = array('first_name', 'last_name', 'email', 'mobile', 'sex', 'gov_identifier', 'pillar', 'category', 'booking_from', 'booking_till', 'nationality', 'faculty_id', 'cluster_id', 'seat_id');
+    
+}
+
+?>
