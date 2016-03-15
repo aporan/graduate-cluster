401f0cb79539194f97c9a21ac1619844febf9e60
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-16 04:07:54 +0800

    controller function for request change table

---
+++ application/controllers/requestchange.php
@@ -0,0 +1,20 @@
+<?php
+
+class Request_Change_Controller extends Base_Controller {
+
+    public $restful = true;
+
+    // renders change request form
+    public function get_index(){
+    }
+
+    // updates student booking
+    public function post_pageone(){
+    }
+
+    public function post_update(){
+    }
+
+}
+
+?>
