21c0cda7bd0cd314a758728d68c5f1f229dc0de8
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-21 03:06:40 +0800

    index page now routes to change request page

--- application/views/static/index.blade.php
+++ application/views/static/index.blade.php
@@ -15,7 +15,7 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('index', 'CHANGE OF DESK', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('change_index', 'CHANGE OF DESK', array(), array('class'=>'button expand')) }}</div>
 	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'ADMIN', array(), array('class'=>'button expand')) }}</div>
 	</div>
       </div>
