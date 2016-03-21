2c81cd30a2815b0dd98eab7400f9e421e5e2b9a3
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-03-21 10:51:31 +0800

    static pages now servers to send email to everybody

--- application/controllers/staticpages.php
+++ application/controllers/staticpages.php
@@ -14,6 +14,17 @@ class StaticPages_Controller extends Base_Controller {
         return View::make('static.admin_index');
     }
 
+    // renders the email page
+    public function get_email_index(){
+        return View::make('static.email_index');
+    }
+
+    // sends a new email after receiving info from the page
+    public function post_email_create(){
+        // validate form entry
+        return Redirect::to('admin');
+    }
+
     // renders login page
     public function get_login(){
         return View::make('static.login');
