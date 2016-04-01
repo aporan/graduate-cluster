b6dadaa2fa9dac8fe76d3a5a4f50e7cd8ba03198
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-02 05:47:46 +0800

    added image upload functionality

--- application/controllers/cluster.php
+++ application/controllers/cluster.php
@@ -50,13 +50,42 @@ class Cluster_Controller extends Base_Controller {
 
 // Helper Functions
 function insertCluster($input){
+    $file_name = parseImagename($input['clusname']);
+    $file_extension = uploadImage($file_name);
+    $image_path = 'img/uploads/'.$file_name.'.'.$file_extension;
+
     Cluster::create(array(
         'cluster_name'=>$input['clusname'],
         'email'=>$input['clusmail'],
         'max_seats'=>$input['clusseats'],
         'level'=>$input['cluslev'],
         'building'=>$input['clusbuild'],
-    ));        
+        'image_path'=>$image_path
+    ));
+}
+
+function uploadImage($name){
+    $img_identifier = 'cluster_image';
+    $file = Input::file($img_identifier);
+    $extension = File::extension($file['name']);
+    $upload_path = 'public/img/uploads';
+    $res = Input::upload($img_identifier, $upload_path, $name.'.'.$extension);
+    if ($res){
+        return $extension;
+    } else {
+        echo "Didn't work!";
+    }
+}
+
+function parseImagename($name){
+    $temp_name = trim($name);
+    $temp_name = strtolower($temp_name);
+    $name_array = explode(" ", $temp_name);
+    $final_name = "";
+    foreach ($name_array as $name){
+        $final_name = $final_name.$name;
+    }
+    return $final_name;
 }
 
 function updateCluster($input){
