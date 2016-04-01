ba33f800af0094cddbc5f0d3fe607098308b6971
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-02 05:48:14 +0800

    updated new page to upload image

--- application/views/cluster/edit.blade.php
+++ application/views/cluster/edit.blade.php
@@ -2,64 +2,76 @@
 
 @section('mainbody')
     <div class="row">
-      <div class="small-2 large-3 columns">
+      <div class="small-12 large-12 columns">
 	<h2>Edit Cluster</h2>
+	<hr/>
       </div>
     </div>
 
-    {{ Form::open('cluster/update', 'PUT') }}
+    <div class="row">
+      <div class="small-12 large-6 columns">
+	{{ Form::open_for_files('cluster/update', 'PUT') }}
         {{ Form::token() }}
 
           <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-10 large-10 columns">
 	      {{ Form::hidden('clus', e($cluster->id)) }}
 	      {{ Form::text('clusname', e($cluster->cluster_name)) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-10 large-10 columns">
               {{ Form::email('clusmail',e($cluster->email)) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-10 large-10 columns">
               {{ Form::text('clusbuild', e($cluster->building)) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-10 large-10 columns">
               {{ Form::text('cluslev', e($cluster->level)) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-10 large-10 columns">
               {{ Form::text('clusseats', e($cluster->max_seats)) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
-              {{ Form::text('image', null, array('placeholder'=>'Image path to be done')) }}
+            <div class="small-10 large-10 columns">
+	      {{ Form::file('cluster_image') }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-2 large-2 columns"></div>
 	  </div>
 
 	  <div class="row">
-            <div class="small-2 large-4 columns">
+            <div class="small-6 large-6 columns">
               {{ Form::submit('UPDATE', array('class'=>'button tiny round')) }}
 	    </div>
-	    <div class="small-10 large-8 columns"></div>
+	    <div class="small-6 large-6 columns"></div>
 	  </div>
 	  
-    {{ Form::close() }}
+	  {{ Form::close() }}
 
+      </div>
+      
+      <div class="small-12 large-6 columns">
+	<h2 class="subheader" style="margin-top: 0px;">Current Preview</h2>
+	{{ HTML::image('img/'.$cluster->image_path) }}
+      </div>
+      
+    </div>  
+    
 @endsection
