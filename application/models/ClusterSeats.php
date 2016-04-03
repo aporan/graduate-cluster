f2a67ee167537eb5d189f8ebf1b40145e4ee1c73
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-03 20:29:02 +0800

    added validation in models using laravels Validation class

--- application/models/ClusterSeats.php
+++ application/models/ClusterSeats.php
@@ -5,6 +5,15 @@ class ClusterSeats extends Eloquent {
     public static $table = 'cluster_seats';
     public static $accessible = array('seat_title', 'available', 'cluster_id');
 
+    public static $rules = array(
+        'cluster' => 'required|integer',
+        'title' => 'required|alpha_dash'
+    );
+
+    public static function validate($data){
+        return Validator::make($data, ClusterSeats::$rules);
+    }
+
 }
 
 ?>
