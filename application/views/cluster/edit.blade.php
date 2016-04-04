9119d4eaf3ff011a29e55a6856043fd58310a902
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-04 17:44:12 +0800

    cleaned up view file to include the partials for a better readability

--- application/views/cluster/edit.blade.php
+++ application/views/cluster/edit.blade.php
@@ -1,6 +1,9 @@
 @layout('base.default')
 
 @section('mainbody')
+
+   {{ render('error.validation') }}
+
     <div class="row">
       <div class="small-12 large-12 columns">
 	<h2>Edit Cluster</h2>
@@ -13,57 +16,17 @@
 	{{ Form::open_for_files('cluster/update', 'PUT') }}
         {{ Form::token() }}
 
-          <div class="row">
-            <div class="small-10 large-10 columns">
-	      {{ Form::hidden('clus', e($cluster->id)) }}
-	      {{ Form::text('clusname', e($cluster->cluster_name)) }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
+	@if($errors->has())
 
-	  <div class="row">
-            <div class="small-10 large-10 columns">
-              {{ Form::email('clusmail',e($cluster->email)) }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
+  	  {{ render('cluster.edit_validation_error', array('cluster'=>$cluster)) }}
 
-	  <div class="row">
-            <div class="small-10 large-10 columns">
-              {{ Form::text('clusbuild', e($cluster->building)) }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
+	@else
 
-	  <div class="row">
-            <div class="small-10 large-10 columns">
-              {{ Form::text('cluslev', e($cluster->level)) }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
-
-	  <div class="row">
-            <div class="small-10 large-10 columns">
-              {{ Form::text('clusseats', e($cluster->max_seats)) }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
-
-	  <div class="row">
-            <div class="small-10 large-10 columns">
-	      {{ Form::file('cluster_image') }}
-	    </div>
-	    <div class="small-2 large-2 columns"></div>
-	  </div>
-
-	  <div class="row">
-            <div class="small-6 large-6 columns">
-              {{ Form::submit('UPDATE', array('class'=>'button tiny round')) }}
-	    </div>
-	    <div class="small-6 large-6 columns"></div>
-	  </div>
+	  {{ render('cluster.edit_partial', array('cluster'=>$cluster)) }}
+	
+	@endif
 	  
-	  {{ Form::close() }}
+	{{ Form::close() }}
 
       </div>
       
@@ -72,6 +35,8 @@
 	{{ HTML::image($cluster->image_path) }}
       </div>
       
-    </div>  
+    </div>
+
+    {{ render('error.validation_js') }}
     
 @endsection
