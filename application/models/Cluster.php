65f5b32bc26396f106a3f74c64f7772587dade34
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-04 17:43:20 +0800

    removed models custom messages as it did not work; added rules for update validation

--- application/models/Cluster.php
+++ application/models/Cluster.php
@@ -6,25 +6,30 @@ class Cluster extends Eloquent {
     public static $accessible = array('cluster_name', 'email', 'max_seats', 'level', 'building', 'image_path');
 
     public static $rules = array(
-        'clusname' => 'required|alpha_dash',
+        'clusname' => 'required|match:/^[A-Za-z0-9\s-_]+$/',
         'clusmail' => 'required|unique:graduate_cluster,email',
         'clusseats' => 'required|integer',
         'cluslev' => 'required|integer',
         'clusbuild' => 'required|alpha_dash',
         'cluster_image' => 'required|image'
     );
-
-    public static $messages = array(
-        'cluslev' => array(
-            'required' => 'Level field is required!',
-            'integer' => 'Level field requires you to input numbers only!'
-        )
+    
+    public static $rules_update = array(
+        'clusname' => 'required|match:/^[A-Za-z0-9\s-_]+$/',
+        'clusseats' => 'required|integer',
+        'cluslev' => 'required|integer',
+        'clusbuild' => 'required|alpha_dash',
+        'cluster_image' => 'required|image'
     );
 
     public static function validate($data){
         return Validator::make($data, Cluster::$rules);
     }
 
+    public static function validate_update($data){
+        return Validator::make($data, Cluster::$rules_update);
+    }
+    
 }
 
 ?>
