679c5b48c1c3dda615a489723a65d4712d71e992
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-04 15:42:14 +0800

    file for displaying validation error message in view

---
+++ application/views/error/validation.blade.php
@@ -0,0 +1,14 @@
+@if($errors->has())
+  <?php $i = 0; ?>
+  @foreach ($errors->all() as $error)
+    <?php $i++ ?>
+    <div id="alerts_{{ $i }}" class="row">
+      <div class="small-12 large-12 columns">
+	<div class="alert-box alert radius">
+	  {{ $error }}<br/>
+	  <a id="trig_{{ $i }}" href="#" class="close" onClick="alertClose(this.id)">⊗</a>
+	</div>
+      </div>
+    </div>
+  @endforeach
+@endif
