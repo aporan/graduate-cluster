011d5fffa6d7d8b9b9c116b8878f7063f1729157
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-04 15:16:46 +0800

    Cluster model now validates data

--- application/models/Cluster.php
+++ application/models/Cluster.php
@@ -5,6 +5,26 @@ class Cluster extends Eloquent {
     public static $table = 'graduate_cluster';
     public static $accessible = array('cluster_name', 'email', 'max_seats', 'level', 'building', 'image_path');
 
+    public static $rules = array(
+        'clusname' => 'required|alpha_dash',
+        'clusmail' => 'required|unique:graduate_cluster,email',
+        'clusseats' => 'required|integer',
+        'cluslev' => 'required|integer',
+        'clusbuild' => 'required|alpha_dash',
+        'cluster_image' => 'required|image'
+    );
+
+    public static $messages = array(
+        'cluslev' => array(
+            'required' => 'Level field is required!',
+            'integer' => 'Level field requires you to input numbers only!'
+        )
+    );
+
+    public static function validate($data){
+        return Validator::make($data, Cluster::$rules);
+    }
+
 }
 
 ?>
