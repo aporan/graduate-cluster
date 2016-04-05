75730973f5589db8345577e33b3ef8f49e692ce4
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-05 10:41:51 +0800

    added validation function in models

--- application/models/Booking.php
+++ application/models/Booking.php
@@ -14,10 +14,43 @@ class Booking extends Eloquent {
         'studtyp' => 'required|in:phd, masters, postdoc, researcher'
     );
 
+    public static $rules_details = array(
+        'studemail' => 'required|email:unique',
+        'studmob' => 'required|integer',
+        'studgov' => 'required|alpha_dash',
+        'bookfro' => 'required|date_format:Y-m-d',
+        'booktill'=> 'required|date_format:Y-m-d',
+        'cluster' => 'required|integer'
+    );
+
+    public static $rules_final = array(
+        'seat' => 'required|integer',
+        'terms' => 'accepted'
+    );
+
+    public static $rules_update = array(
+        'studfirst' => 'required|alpha',
+        'studlast' => 'required|alpha',
+        'studmob' => 'required|integer',
+        'bookfro' => 'required|date_format:Y-m-d',
+        'booktill'=> 'required|date_format:Y-m-d',
+    );
+
     public static function validation_basic($data){
         return Validator::make($data, Booking::$rules_basic);
     }
-    
+
+    public static function validation_details($data){
+        return Validator::make($data, Booking::$rules_details);
+    }
+
+    public static function validation_final($data){
+        return Validator::make($data, Booking::$rules_final);
+    }
+
+    public static function validation_update($data){
+        return Validator::make($data, Booking::$rules_update);
+    }
 }
 
 ?>
