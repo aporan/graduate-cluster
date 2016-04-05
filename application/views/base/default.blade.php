8aef0e5592e2890c17511730b8a2ec5eca0b2e35
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-05 10:42:34 +0800

    added alert box for session messages

--- application/views/base/default.blade.php
+++ application/views/base/default.blade.php
@@ -49,10 +49,29 @@
 
 <body>
   @if(Session::has('message'))
-    {{ Session::get('message') }}
+    <div id="alert" class="row">
+      <div class="small-12 large-12 columns">
+	<div class="alert-box success radius">
+	  {{ Session::get('message') }}
+	  <a id="trig" href="#" class="close" onClick="alertClose(this.id)">⊗</a>
+	</div>
+      </div>
+    </div>
   @endif
   
   @yield('mainbody')
+
+  @if(Session::has('message'))
+    <script>
+      function alertClose($id){
+        var alert_id = $("#"+$id).closest("div").parent().parent().attr("id");
+        $("#"+alert_id).slideUp("slow", function(){
+            $(this).remove();
+        });
+      }
+    </script>
+  @endif  
+    
 </body>
      
 </html>
