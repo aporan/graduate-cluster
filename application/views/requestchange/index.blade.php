7e65694a3c48183618a85973be3fc35a89d2196a
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-13 22:37:29 +0800

    fixed code formatting for proper flash notification width

--- application/views/requestchange/index.blade.php
+++ application/views/requestchange/index.blade.php
@@ -1,44 +1,49 @@
 @layout('base.default')
 
 @section('mainbody')
-  <div class="row" style="margin-top: 30px;">
-    <h2>Request for Change of Desk Space</h2>
-    <hr/>
+  <div class="row">
+    <div class="small-12 large-12 columns">
+      <h2>Request for Change of Desk Space</h2>
+      <hr/>
+    </div>
   </div>
 
   <div class="row">
-    <table>
-      <thead>
-	<tr>
-	  <th>#</th>
-	  <th width="11%">Name</th>
-	  <th>Email</th>
-	  <th class="text-center">Contact</th>
-	  <th class="text-center">Pillar</th>
-	  <th class="text-center">Category</th>
-	  <th class="text-center">Booking From</th>
-	  <th class="text-center">Booking Date</th>
-	  <th class="text-center">Nationality</th>
-	  <!-- TODO: Group by Graduate Cluster -->
-	  <th class="text-center">Graduate Center</th>
-	</tr>
-      </thead>
-      <tbody>
-	<?php $i = 1; ?>
-	@foreach($bookings as $booking)
+    <div class="small-12 large-12 columns">
+      <table>
+	<thead>
 	  <tr>
-	    <td>{{ $i++ }}</td>
-	    <td>{{ HTML::link_to_route('new_change', e($booking->first_name).e(" ".$booking->last_name) , array($booking->id)) }}</td>
-	    <td>{{ e($booking->email) }}</td>
-	    <td class="text-center">{{ e($booking->mobile) }}</td>
-	    <td class="text-center">{{ e($booking->pillar) }}</td>
-	    <td class="text-center">{{ e($booking->category) }}</td>
-	    <td class="text-center">{{ e($booking->booking_from) }}</td>
-	    <td class="text-center">{{ e($booking->booking_till) }}</td>
-	    <td class="text-center">{{ e($booking->nationality) }}
-	    <td class="text-center">{{ e(getClusterName($booking->cluster_id)) }}</td>
+	    <th>#</th>
+	    <th width="11%">Name</th>
+	    <th>Email</th>
+	    <th class="text-center">Contact</th>
+	    <th class="text-center">Pillar</th>
+	    <th class="text-center">Category</th>
+	    <th class="text-center">Booking From</th>
+	    <th class="text-center">Booking Date</th>
+	    <th class="text-center">Nationality</th>
+	    <!-- TODO: Group by Graduate Cluster -->
+	    <th class="text-center">Graduate Center</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  <?php $i = 1; ?>
+	  @foreach($bookings as $booking)
+	    <tr>
+	      <td>{{ $i++ }}</td>
+	      <td>{{ HTML::link_to_route('new_change', e($booking->first_name).e(" ".$booking->last_name) , array($booking->id)) }}</td>
+	      <td>{{ e($booking->email) }}</td>
+	      <td class="text-center">{{ e($booking->mobile) }}</td>
+	      <td class="text-center">{{ e($booking->pillar) }}</td>
+	      <td class="text-center">{{ e($booking->category) }}</td>
+	      <td class="text-center">{{ e($booking->booking_from) }}</td>
+	      <td class="text-center">{{ e($booking->booking_till) }}</td>
+	      <td class="text-center">{{ e($booking->nationality) }}
+	      <td class="text-center">{{ e(getClusterName($booking->cluster_id)) }}</td>
+	    </tr>
 	  @endforeach
-      </tbody>
-    </table>
+	</tbody>
+      </table>
+    </div>
   </div>
 @endsection
