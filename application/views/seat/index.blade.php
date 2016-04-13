b40007bd037f7d7538d7c9b5dd03b7c518a2c023
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-13 22:39:07 +0800

    fixed code formatting for proper flash notification width

--- application/views/seat/index.blade.php
+++ application/views/seat/index.blade.php
@@ -2,39 +2,47 @@
 
 @section('mainbody')
     <div class="row">
-      <h2>{{ $this_cluster->cluster_name }}</h2>
-      <hr/>
+      <div class="small-12 large-12 columns">
+	<h2>{{ $this_cluster->cluster_name }}</h2>
+	<hr/>
+      </div>
     </div>
 
-    <div class="row"><h4 class="subheader">List of Desk Spaces :</h4></div>
     <div class="row">
-      <table>
-	<thead>
-	  <tr>
-	    <th>#</th>
-	    <th> Number / Title</th>
-	    <th width="20%"></th>
-	  </tr>
-	</thead>
-	<tbody>
-	  <?php $i = 1; ?>
-	  @foreach($seats as $seat)
-   	    <tr>
-	      <td>{{ $i++ }}</td>
-	      <td>{{ $seat->seat_title }}</td>
-	      <td>
- 	        {{ Form::open('seat/delete', 'DELETE', array('style'=>'padding-top:20px;')) }}
-  	        {{ Form::hidden('id', e($seat->id)) }}
-		{{ Form::hidden('clusid', e($this_cluster->id)) }}
-	        {{ Form::submit('Delete', array('class'=>'button tiny alert round')) }}
-	        {{ Form::close() }}
-	      </td>
-	    </tr>
-	  @endforeach
-	</tbody>
-      </table>
+      <div class="small-12 large-12 columns">
+	<h4 class="subheader">List of Desk Spaces :</h4>
+      </div>
     </div>
-
-
+    
+    <div class="row">
+      <div class="small-12 large-12 columns">
+	<table>
+	  <thead>
+	    <tr>
+	      <th>#</th>
+	      <th> Number / Title</th>
+	      <th width="20%"></th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <?php $i = 1; ?>
+	    @foreach($seats as $seat)
+   	      <tr>
+		<td>{{ $i++ }}</td>
+		<td>{{ $seat->seat_title }}</td>
+		<td>
+ 	          {{ Form::open('seat/delete', 'DELETE', array('style'=>'padding-top:20px;')) }}
+  	          {{ Form::hidden('id', e($seat->id)) }}
+		  {{ Form::hidden('clusid', e($this_cluster->id)) }}
+	          {{ Form::submit('Delete', array('class'=>'button tiny alert round')) }}
+	          {{ Form::close() }}
+		</td>
+	      </tr>
+	    @endforeach
+	  </tbody>
+	</table>
+      </div>
+    </div>
     
 @endsection
+    
