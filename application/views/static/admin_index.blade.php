80ea2a462c36d0ca49d01941f9c9350f1986a6d9
AhnafSiddiqi <ahnafsidd@gmail.com>
2016-04-14 10:27:48 +0800

    added custom class to button for fancy animation

--- application/views/static/admin_index.blade.php
+++ application/views/static/admin_index.blade.php
@@ -5,8 +5,8 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button expand')) }}</div>
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button expand main')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button expand main')) }}</div>
 	</div>
       </div>
     </div>
@@ -14,8 +14,8 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'REPORT PAGE', array(), array('class'=>'button expand')) }}</div>
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('email_index', 'SEND EMAILS', array(), array('class'=>'button expand')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'REPORT PAGE', array(), array('class'=>'button expand main')) }}</div>
+	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('email_index', 'SEND EMAILS', array(), array('class'=>'button expand main')) }}</div>
 	</div>
       </div>
     </div>
