2e71a8102dc1206c19825569e482bb46440549b3
ahnaf <ahnafsidd@gmail.com>
2016-06-29 18:40:52 +0800

    for readability

--- application/controllers/seat.php
+++ application/controllers/seat.php
@@ -28,9 +28,15 @@ class Seat_Controller extends Base_Controller {
         $validation = ClusterSeats::validate($input);
 
         if ($validation->fails()){
+            
             $clusters = Cluster::order_by('id')->lists('cluster_name', 'id');
-            return Redirect::to_route('new_seat')->with_errors($validation)->with_input()->with('clusters', $clusters); 
+            return Redirect::to_route('new_seat')
+                ->with_errors($validation)
+                ->with_input()
+                ->with('clusters', $clusters);
+            
         } else {
+            
             insertSeat($input);
             $message = "Seat was succesfully created!";
             return Redirect::to_route('cluster_seats', array($cluster_id))
