cfa430b49ec7d99cc3d7c4f556851c9207063a9e
ahnaf <ahnafsidd@gmail.com>
2016-06-29 18:43:32 +0800

    added validation for user registration

--- application/models/User.php
+++ application/models/User.php
@@ -5,6 +5,23 @@ class User extends Eloquent {
     public static $table = 'users';
     public static $accessbile = array('first_name', 'last_name', 'email', 'password', 'type');
 
+    public static $rules = array(
+        'firstname' => 'required|match:/^[A-Za-z0-9\s-_ ]+$/',
+        'lastname' => 'required|match:/^[A-Za-z0-9\s-_ ]+$/',
+        'email' => 'required|unique: users, email',
+        'password'=>'confirmed'
+    );
+
+    public static function validate($data){
+        return Validator::make($data, User::$rules);
+    }
+
+    public function bookings(){
+        return $this->has_many('Booking');
+    }
+
+
+
 }
 
 ?>
