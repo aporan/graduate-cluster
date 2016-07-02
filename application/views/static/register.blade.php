144d136018ada1db64b57dc72c66641c5453dba7
ahnaf <ahnafsidd@gmail.com>
2016-07-02 11:27:43 +0800

    added proper blade content for registration

--- application/views/static/register.blade.php
+++ application/views/static/register.blade.php
@@ -2,59 +2,65 @@
 
 @section('mainbody')
 
-{{ render('error.validation') }}
-
-<div class="large-4 large-centered columns">
-  <div class="login-box">
+<div class="row register">
+  <div class="large-5 columns">
+    <div class="login-box">
     
-    {{ Form::open('register/create', 'POST') }}
-    {{ Form::token() }}
+      {{ Form::open('register/create', 'POST') }}
+      {{ Form::token() }}
           
-      <div class="row">
-	<div class="large-12 columns">
-          {{ Form::label('firstname', 'First Name') }}
-	  {{ Form::text('firstname', Input::old('firstname')) }}
-	</div>
-      </div>
+        <div class="row">
+	  <div class="large-12 columns">
+            {{ Form::label('firstname', 'First Name') }}
+	    {{ Form::text('firstname', Input::old('firstname')) }}
+	  </div>
+        </div>
 
-      <div class="row">
-	<div class="large-12 columns">
-          {{ Form::label('lastname', 'Last Name') }}
-	  {{ Form::text('lastname', Input::old('lastname')) }}
-	</div>
-      </div>
+        <div class="row">
+	  <div class="large-12 columns">
+            {{ Form::label('lastname', 'Last Name') }}
+	    {{ Form::text('lastname', Input::old('lastname')) }}
+	  </div>
+        </div>
 
-      <div class="row">
-	<div class="large-12 columns">
-          {{ Form::label('email', 'Email') }}
-	  {{ Form::text('email', Input::old('email')) }}
-	</div>
-      </div>
+        <div class="row">
+	  <div class="large-12 columns">
+            {{ Form::label('email', 'Email') }}
+	    {{ Form::text('email', Input::old('email')) }}
+	  </div>
+        </div>
 
-      <div class="row">
-	<div class="large-12 columns">
-          {{ Form::label('password', 'Password') }}
-	  {{ Form::password('password') }}
-	</div>
-      </div>
-
-      <div class="row">
-	<div class="large-12 columns">
-          {{ Form::label('password_confirmation', 'Confirm Password') }}
-	  {{ Form::password('password_confirmation') }}
-	</div>
-      </div>
+        <div class="row">
+	  <div class="large-12 columns">
+            {{ Form::label('password', 'Password') }}
+	    {{ Form::password('password') }}
+	  </div>
+        </div>
+        
+        <div class="row">
+	  <div class="large-12 columns">
+            {{ Form::label('password_confirmation', 'Confirm Password') }}
+	    {{ Form::password('password_confirmation') }}
+	  </div>
+        </div>
           
-      <div class="row">
-	<div class="large-12 large-centered columns">
-	  <input type="submit" class="button expand" value="Log In"/>
-	</div>
-      </div>
-	  
-
-    {{ Form::close() }}
-
+        <div class="row">
+	  <div class="large-12 large-centered columns">
+	    <input type="submit" class="button expand" value="Log In"/>
+	  </div>
+        </div>
+	
+        
+      {{ Form::close() }}
+        
+    </div>
   </div>
+
+  <div class="large-1 columns"></div>
+  <div class="large-6 columns">
+    {{ render('error.validation') }}
+  </div>
+
 </div>
 
 {{ render('error.validation_js') }}
