38c629d37566286edf1628053243b91c7e9bd907
ahnaf <ahnafsidd@gmail.com>
2016-07-02 11:25:33 +0800

    added validation for Users model regarding authentication, login, and registration

--- application/models/User.php
+++ application/models/User.php
@@ -5,23 +5,45 @@ class User extends Eloquent {
     public static $table = 'users';
     public static $accessbile = array('first_name', 'last_name', 'email', 'password', 'type');
 
-    public static $rules = array(
+    public static $rules_login = array(
+        'email'=> 'required|email',
+        'password'=> 'required|min:5'
+    );
+
+    public static $rules_registration = array(
         'firstname' => 'required|match:/^[A-Za-z0-9\s-_ ]+$/',
         'lastname' => 'required|match:/^[A-Za-z0-9\s-_ ]+$/',
         'email' => 'required|unique: users, email',
-        'password'=>'confirmed'
+        'password'=>'required|confirmed|alpha_num|min:5'
     );
 
-    public static function validate($data){
-        return Validator::make($data, User::$rules);
+    public static $rules_verify_email = array(
+        'email' => 'required|email',
+    );
+
+    public static $rules_reset_password = array(
+        'password' => 'required|confirmed|alpha_num|min:5'
+    );
+
+    public static function validateLogin($data) {
+        return Validator::make($data, User::$rules_login);
     }
 
-    public function bookings(){
+    public static function validateRegistration($data) {
+        return Validator::make($data, User::$rules_registration);
+    }
+
+    public static function verifyEmail($data) {
+        return Validator::make($data, User::$rules_verify_email);
+    }
+
+    public static function validateNewPassword($data) {
+        return Validator::make($data, User::$rules_reset_password);
+    }
+
+    public function bookings() {
         return $this->has_many('Booking');
     }
-
-
-
 }
 
 ?>
