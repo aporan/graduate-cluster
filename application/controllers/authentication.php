1f22242f3e536b740ac0d57545cdd97e66fcfc22
ahnaf <ahnafsidd@gmail.com>
2016-07-02 11:21:56 +0800

    added authentication helpers for registration, reset, and login

--- application/controllers/authentication.php
+++ application/controllers/authentication.php
@@ -1,34 +1,122 @@
 <?php
 
+include(path('app').'/helpers/authentication.php');
+
 class Authentication_Controller extends Base_Controller {
 
     public $restful = true;
 
-    // renders login page
-    public function get_login(){
-        return View::make('static.login');
-    }
-
-    public function get_register(){
-        return View::make('static.register');
-    }
-
-    public function post_create(){
+    public function get_view_login() {  return View::make('static.login');  }
+    
+    public function post_login() {
         $input = Input::all();
-        $validation = User::validate($input);
+        $validation = User::validateLogin($input);
 
-        if ($validation->fails()){
+        if ($validation->fails()) {
+
+            return Redirect::to_route('login')
+                ->with_errors($validation);
+            
+        } else {
+            
+            $email = $input['email'];
+            $password = $input['password'];
+            $credentials = array('username'=>$email, 'password'=>$password);
+            if (Auth::attempt($credentials)) {
+
+                $user = User::where_email($email)->first();
+                $message = 'Hi <b>'. $user->first_name .'</b>! Glad you could make it.';
+                return Redirect::to_route('index')
+                    ->with('message', $message);
+
+            } else {
+
+                $error = array('Login'=>'Invalid <code><b>Email / Password</b></code> Combination');
+                $invalid_credentials = new Messages($error);
+                return Redirect::to_route('login')
+                    ->with_errors($invalid_credentials);
+            }
+        }
+    }
+
+    public function get_view_register() {  return View::make('static.register');  }
+
+    public function post_register() {
+        $input = Input::all();
+        $validation = User::validateRegistration($input);
+
+        if ($validation->fails()) {
 
             return Redirect::to_route('register')
                 ->with_errors($validation)
                 ->with_input();
 
         } else {
-            echo "It works!";
+
+            insertUser($input);
+            sendRegistrationEmail($input);
+            $message = "Registration is successful! Please login to access the website";
+            return Redirect::to_route('login')
+                ->with('message', $message);
+              
         }
     }
 
+    public function post_logout() {
+        if (Auth::check()) {
+            Auth::logout();
+            return Redirect::to_route('login');
+        }
+    }
 
+    public function get_view_verify() {  return View::make('static.verify');  }
+
+    public function post_verify() {
+        $input = Input::all();
+        $validation = User::verifyEmail($input);
+
+        if ($validation->fails()) {
+
+            Return Redirect::to_route('verify')
+                ->with_errors($validation);
+
+        } else {
+
+            $email = $input['email'];
+            if (findUser($email)) {
+
+                storeUserEmail($email);
+                return Redirect::to_route('reset');
+                    
+            } else {
+                
+                $error = array('Login'=>'<code><b>Email</b></code> is not registered! Please Sign Up.');
+                $invalid_credentials = new Messages($error);
+                return Redirect::to_route('verify')
+                    ->with_errors($invalid_credentials);                
+            }
+        }
+    }
+
+    public function get_view_reset() {  return View::make('static.reset');  }
+
+    public function post_reset() {
+        $input = Input::all();
+        $validation = User::validateNewPassword($input);
+
+        if ($validation->fails()) {
+
+            return Redirect::to_route('reset')
+                ->with_errors($validation);
+
+        } else {
+
+            changePassword($input);
+            $message = "Password has been updated! Please login to continue.";
+            return Redirect::to_route('login')
+                ->with('message', $message);
+        }
+    }
 }
 
 ?>
