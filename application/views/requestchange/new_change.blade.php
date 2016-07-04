5fe7b5ef331ec3a57fe7f0dd6559d81862f69246
ahnaf <ahnafsidd@gmail.com>
2016-07-04 12:04:50 +0800

    added image path; removed terms and conditions

--- application/views/requestchange/new_change.blade.php
+++ application/views/requestchange/new_change.blade.php
@@ -14,6 +14,12 @@
 	<hr/>
       </div>
     </div>
+
+    <div class="row">
+      <div class="small-12 large-12 columns">
+	{{  HTML::image($path) }}
+      </div>
+    </div>
     
     <div class="row">
       <div class="small-2 large-3 columns">
@@ -25,12 +31,6 @@
       <div class="small-2 large-5 columns"></div>
     </div>
 
-    <div class="row">
-      <div class="large-12 columns">
-	<h4 class="subheader">1. Booking Duration</h4>
-      </div>
-    </div>
-      
     <div class="row">
       <div class="large-2 columns">
 	{{ Form::label('bookfro', 'Booking From:', array('class'=>'required', 'style'=>'padding-top: 7px')) }}
@@ -44,20 +44,6 @@
       <div class="large-3 columns end">
 	{{ Form::text('booktill', null, array('placeholder'=>'yy-mm-dd', 'id'=>'to')) }}
       </div>
-    </div>
-
-    <div class="row">
-      <div class="large-12 columns">
-	<h4 class="subheader">2. Terms and Conditions </h4>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="small-6 large-10 columns">
-      	{{ Form::checkbox('terms', '1') }}
-	<span>Please check the terms and condtions before proceeding</span>
-      </div>
-      <div class="small-6 large-2 columns"></div>
     </div>
 
     <div class="row">
