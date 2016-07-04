e19c92b17bbeab993e3c30e4da5c1ad79e714dd6
ahnaf <ahnafsidd@gmail.com>
2016-07-04 12:05:35 +0800

    updated admin index page to add users functionality

--- application/views/static/admin_index.blade.php
+++ application/views/static/admin_index.blade.php
@@ -14,8 +14,7 @@
     <div class="row">
       <div class="small-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('admin_index', 'REPORT PAGE', array(), array('class'=>'button expand main')) }}</div>
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('email_index', 'SEND EMAILS', array(), array('class'=>'button expand main')) }}</div>
+	  <div class="large-12 columns">{{ HTML::link_to_ROUTE('manager_index', 'MANAGE USERS', array(), array('class'=>'button expand main')) }}</div>
 	</div>
       </div>
     </div>
