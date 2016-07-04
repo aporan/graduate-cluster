66b883ed6394cb69d0957340e1a5427dca325d09
ahnaf <ahnafsidd@gmail.com>
2016-07-04 12:00:29 +0800

    updated navbar to add admin button; made it look nicer

--- application/views/base/navbar.blade.php
+++ application/views/base/navbar.blade.php
@@ -2,7 +2,7 @@
 
   <!-- Foundation suggests to keep it even if the title is not required -->
   <ul class="title-area">
-    <li class="name">{{ HTML::link_to_ROUTE('index', 'BOOKING', array(), array()) }}</li>
+    <li class="name">{{ HTML::link_to_ROUTE('index', 'BOOKING') }}</li>
   </ul>
     
   <section class="top-bar-section">
@@ -12,19 +12,31 @@
     </ul>
 
     <ul class="right">    <!-- Right Nav Section -->
-      <!-- <li class="divider"></li> -->
-      <li class="has-form">
-        @if (Auth::check())
-          {{ Form::open('logout', 'POST') }}
-          {{ Form::token() }}
-        
-          <button type="submit">
-            <i class="fi-lock"></i>Sign Out
-          </button>
-          {{ Form::close() }}
-        @endif
-      </li>
+      @if (Auth::check())
+
+         @if (Auth::user()->type == 'admin') 
+           <li class="has-form">
+             <a class="button" href={{ URL::to_route('admin_index') }}>
+               <i class="fi-widget"></i>Admin
+             </a>
+           </li>
+
+           <li class="divider"></li>
+         @endif
       
+         <li class="has-form">
+
+           {{ Form::open('logout', 'POST') }}
+           {{ Form::token() }}
+        
+           <button type="submit">
+             <i class="fi-lock"></i>Sign Out
+           </button>
+           {{ Form::close() }}
+
+         </li>
+
+      @endif
     </ul>
     
   </section>
