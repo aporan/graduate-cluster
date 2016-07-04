e0d953a7f8c60d2dd6aa466a01f781280ffaa4e6
ahnaf <ahnafsidd@gmail.com>
2016-07-04 12:04:17 +0800

    change of requests bookings are now shown according to cluster groups

--- application/views/requestchange/index.blade.php
+++ application/views/requestchange/index.blade.php
@@ -10,40 +10,62 @@
 
   <div class="row">
     <div class="small-12 large-12 columns">
-      <table>
-	<thead>
-	  <tr>
-	    <th>#</th>
-	    <th width="11%">Name</th>
-	    <th>Email</th>
-	    <th class="text-center">Contact</th>
-	    <th class="text-center">Pillar</th>
-	    <th class="text-center">Category</th>
-	    <th class="text-center">Booking From</th>
-	    <th class="text-center">Booking Date</th>
-	    <th class="text-center">Nationality</th>
-	    <!-- TODO: Group by Graduate Cluster -->
-	    <th class="text-center">Graduate Center</th>
-	  </tr>
-	</thead>
-	<tbody>
-	  <?php $i = 1; ?>
-	  @foreach($bookings as $booking)
-	    <tr>
-	      <td>{{ $i++ }}</td>
-	      <td>{{ HTML::link_to_route('new_change', e($booking->first_name).e(" ".$booking->last_name) , array($booking->id)) }}</td>
-	      <td>{{ e($booking->email) }}</td>
-	      <td class="text-center">{{ e($booking->mobile) }}</td>
-	      <td class="text-center">{{ e($booking->pillar) }}</td>
-	      <td class="text-center">{{ e($booking->category) }}</td>
-	      <td class="text-center">{{ e($booking->booking_from) }}</td>
-	      <td class="text-center">{{ e($booking->booking_till) }}</td>
-	      <td class="text-center">{{ e($booking->nationality) }}
-	      <td class="text-center">{{ e(getClusterName($booking->cluster_id)) }}</td>
-	    </tr>
-	  @endforeach
-	</tbody>
-      </table>
+      <ul class="accordion" data-accordion>
+        <?php $j= 1; ?>
+        @foreach($cluster_bookings as $cluster_id => $bookings)
+          @if (!empty($bookings))
+        
+            <li class="accordion-navigation">
+              <a href="#panel{{ $j }}a"><h4>{{ Cluster::find($cluster_id)->name }}</h4></a>
+              <div id="panel{{ $j }}a" class="content active">
+                <?php $j++?>
+                <div class="row">
+                  <div class="small-12 large-12 columns">
+                    <table> 
+	              <thead>
+	                <tr>
+	                  <th>#</th>
+	                  <th>Name</th>
+                          <th class="text-center">Pillar</th>
+	                  <th class="text-center">Category</th>
+	                  <th class="text-center">Booking From</th>
+	                  <th class="text-center">Booking Till</th>
+	                  <th class="text-center">Nationality</th>
+	                  <th class="text-center">Seat</th>
+	                </tr>
+	              </thead>
+                      
+	              <tbody>
+	                <?php $i = 1; ?>
+	                @foreach($bookings as $booking)
+   	                  <tr>
+                            <td>{{ $i++ }}</td>
+	                    <td>{{ HTML::link_to_route('new_change', e(ucwords($booking->first_name)).e(" ".ucwords($booking->last_name)) , array($booking->id)) }}</td>
+	                    <td class="text-center">{{ e(strtoupper($booking->pillar)) }}</td>
+	                    <td class="text-center">{{ e(ucwords($booking->category)) }}</td>
+                            <td class="text-center">{{ date_format(date_create(e($booking->booking_from)),'Y-m-d') }}</td>
+                            <td class="text-center">{{ date_format(date_create(e($booking->booking_till)),'Y-m-d') }}</td>
+	                    <td class="text-center">{{ e($booking->nationality) }}
+                            <td class="text-center">{{ e(ClusterSeat::find($booking->seat_id)->number) }}</td>
+	                  </tr>
+	                @endforeach
+	              </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </li>
+          @endif
+        @endforeach
+      </ul>
     </div>
   </div>
+
+
+<script>
+  $(document).ready(function(){
+      $(document).foundation();
+  });
+</script>
 @endsection
+
