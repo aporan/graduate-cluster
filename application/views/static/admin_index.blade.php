2d785863217cc59685dc5f0720f25aa1688b4c91
ahnaf <ahnafsidd@gmail.com>
2016-07-19 02:38:04 +0800

    added user registration routes in the admin page

--- application/views/static/admin_index.blade.php
+++ application/views/static/admin_index.blade.php
@@ -3,18 +3,19 @@
 @section('mainbody')
   <div class="page-centre">
     <div class="row">
-      <div class="small-7 large-centered columns">
+      <div class="small-12 large-7 large-centered columns">
 	<div class="row">
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button expand main')) }}</div>
-	  <div class="small-2 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button expand main')) }}</div>
+	  <div class="small-12 large-6 columns">{{ HTML::link_to_ROUTE('clusters', 'GRADUATE CENTERS', array(), array('class'=>'button expand main')) }}</div>
+	  <div class="small-12 large-6 columns">{{ HTML::link_to_ROUTE('new_seat', 'ADD DESK SPACE', array(), array('class'=>'button expand main')) }}</div>
 	</div>
       </div>
     </div>
 
     <div class="row">
-      <div class="small-7 large-centered columns">
+      <div class="small-12 large-7 large-centered columns">
 	<div class="row">
 	  <div class="large-12 columns">{{ HTML::link_to_ROUTE('manager_index', 'MANAGE USERS', array(), array('class'=>'button expand main')) }}</div>
+          <div class="large-12 columns">{{ HTML::link_to_ROUTE('register', 'REGISTER USERS', array(), array('class'=>'button expand main')) }}</div>
 	</div>
       </div>
     </div>
